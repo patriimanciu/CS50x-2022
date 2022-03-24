3b2a205be0853f60a5661d3b2466b20ed9cd841f
patriimanciu <patri.manciu@gmail.com>
2022-03-24 21:19:06 +0200

    add lab5

--- pset5/inheritance/inheritance.c
+++ pset5/inheritance/inheritance.c
@@ -50,8 +50,12 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
+        prs->parents[0] = parent0;
+        prs->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        prs->alleles[0] = prs->parents[0]->alleles[rand() % 2];
+        prs->alleles[1] = prs->parents[1]->alleles[rand() % 2];
 
     }
 
@@ -59,26 +63,32 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
-        prs->parent[0] = NULL;
-        prs->parent[1] = NULL;
+        prs->parents[0] = NULL;
+        prs->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
-
+        prs->alleles[0] = random_allele();
+        prs->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
-    return NULL;
+    return prs;
 }
 
 // Free `p` and all ancestors of `p`.
 void free_family(person *p)
 {
     // TODO: Handle base case
-
+    if (p == NULL)
+    {
+        return;
+    }
     // TODO: Free parents recursively
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
 
     // TODO: Free child
-
+    free(p);
 }
 
 // Print each family member and their alleles.
